622315ab58da6b68e6d5ebfdf8204e5f92374453
MinwooHconnect <kmwdev@hconnect.co.kr>
2023-09-08 15:55:19 +0900

    - 블루투스 기능 추가

--- lib/hcm_core.dart
+++ lib/hcm_core.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:hcm_core/utils/enum_health_index.dart';
 import 'package:hcm_core/widget/card_blood_sugar_risk.dart';
 
@@ -6,6 +9,19 @@ class HcmCore {
   // Future<String?> getPlatformVersion() {
   //   return HcmCorePlatform.instance.getPlatformVersion();
   // }
+}
+
+class HcmCoreBle extends FlutterBluePlus {
+  static final adapterState = FlutterBluePlus.adapterState;
+  static final scanResults = FlutterBluePlus.scanResults;
+
+  static Future startScan(int timeout) {
+    return FlutterBluePlus.startScan(
+        timeout: Duration(seconds: timeout),
+        androidUsesFineLocation: Platform.isAndroid);
+  }
+
+  static final isScanning = FlutterBluePlus.isScanning;
 }
 
 class HcmCoreUI {
